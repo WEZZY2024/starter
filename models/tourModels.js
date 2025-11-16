3393bf42ca2d9d0c27bb8568ddad2e8fed4b5787
wezzy ngulube <ngulubewezzy@outlook.com>
2025-11-17 01:01:24 +0200

    add aggregation middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -86,8 +86,13 @@ tourSchema.pre(/^find/,function(next){
 });
 tourSchema.post(/^find/,function(doc,next){
     next();
-})
+}) 
 
+// AGGREGATION MIDDLEWARE 
+tourSchema.pre('aggregate', function(next){
+   this.pipeline().unshift({ $match : {secretTour : {$ne : true}}});
+   next(); 
+})
 
 
 const Tour = mongoose.model('Tour',tourSchema);
