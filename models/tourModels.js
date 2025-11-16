4518bc6d84e761bd7978483841ea165f158929fa
wezzy ngulube <ngulubewezzy@outlook.com>
2025-11-16 21:38:15 +0200

    worked

--- models/tourModels.js
+++ models/tourModels.js
@@ -7,6 +7,7 @@ const tourSchema = new mongoose.Schema({
         unique : true,
         trim: true
     },
+    slug : String,
     durations : {
         type : Number,
         required : [true, 'A tour must have a duration']
@@ -61,7 +62,17 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration / 7
 })
-
+//DOCUMENT MIDDLEWARE : runs before .save() and .create()
+tourSchema.pre('save',function(next){
+this.slug = slugify(this.name,{ lower:true })
+next();  
+})
+// tourSchema.pre('save',function(next){
+//     next( )
+// })
+// tourSchema.post('save',function(doc,next){
+//     next();
+// })
 const Tour = mongoose.model('Tour',tourSchema);
 
 module.exports = Tour 
