969624c3c4c4c11486540532d599473b0d78cf5d
wezzy ngulube <ngulubewezzy@outlook.com>
2025-12-04 12:16:08 +0200

    mongoose error

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,5 +1,8 @@
 const AppError = require('../utils/appError');
-
+ const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path} : ${err.value}`;
+    return new AppError(message,400);
+ }
 const sendErrorDev = (err , res ) => {
   res.status(err.statusCode).json({
     status : err.status,
@@ -27,17 +30,14 @@ const sendErrorProd = (err , res) => {
   })
 }
 }
-
-
 module.exports = (err,req,res,next) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
+  err.status = err.status || 'error'; 
    if(process.env.NODE_ENV === 'development'){
     sendErrorDev(err , res);
-  
-
    } else if(process.env.NODE_ENV === 'production'){
-      sendErrorProd(err , res)
+     let error = {...err };
+      if(error.name === 'CastError') error = handleCastErrorDB(error)
+      sendErrorProd(error , res)
    }
   }
